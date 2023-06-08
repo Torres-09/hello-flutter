a93f44be3242b840cdc1f6f4e6d82590eeb41d29
hwan2da <dla612@naver.com>
2023-06-09 01:13:39 +0900

    Padding and magin

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,23 @@ class MyApp extends StatelessWidget {
             style: TextStyle(fontSize: 30),
           ),
         ),
-        body: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "email"),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: "password"),
-            ),
-            ElevatedButton(onPressed: () {}, child: Text('Login'))
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "email"),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: "password"),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 16),
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: () {}, child: Text('Login')))
+            ],
+          ),
         ),
       ),
     );
